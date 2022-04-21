4b366f3be661b5510eb38a15306add3738e268db
alainforton@gmail.com <alainforton@gmail.com>
2022-04-21 16:20:33 -0500

    5RTD

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # log3
+
+## 20220421 prtAngel 
+cell motog7 conectado/ TODO win7
+
+readme temporal hacer su propio
+falta avoch³ss, establecido magdr3z, opcional PLfriedman o sinst
+
+notas, posbiblr testing previo a dp PPrin 
+
---
+++ tournament/19600515moscow.js
@@ -0,0 +1,9 @@
+//Tal book champ match vs botvinick
+// TODO show text one side play other
+// leer archivo que tendra el archivo mostrar a traves de alguna regla el archivo, me imagino array bidi, donde cada casilla y luego si variante si tiene.
+// lector de png ? como leer la partida del ong o los distintos datos 
+// fen ?? idea de los hilos, como un tapiz que fluctua. 
+// fen ??? lector normal, incluir comntarios.
+// first eng como en el book
+// play by play, tres lang es, en, ru, proyectado a hindi y arab/jap
+
